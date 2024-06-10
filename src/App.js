3435fe4127548b343dba88e8988fe0eca0673996
조연경 <sig.yg5057@gmail.com>
2024-06-10 12:37:43 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
 
 import MainLayout from './layout/Main';
 import LandingMain from './pages/LandingMain'
 
 function App() {
-  const navigate = useNavigate();
+  const location = useLocation();
 
-  useEffect(() => { navigate('/survey'); }, [navigate]);
-
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag('config', 'G-4JM33X75PS', {
+        page_path: location.pathname,
+      });
+    }
+  }, [location]);
   return (
     <MainLayout>
       <Routes>
-        <Route path="/" element={<LandingMain />} />
+        <Route path="/" element={<Navigate to="/survey" />} />
         <Route path="/survey" element={<LandingMain />} />
       </Routes>
     </MainLayout>
